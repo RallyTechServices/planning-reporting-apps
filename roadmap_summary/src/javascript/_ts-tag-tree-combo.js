8f4dbc6e4bdf732ac0a8216c02347175580272ea
John Martin <curmudgeon+git@rallydev.com>
2015-03-25 20:40:17 -0700

    add a tag selector, switch to features

---
+++ roadmap_summary/src/javascript/_ts-tag-tree-combo.js
@@ -0,0 +1,143 @@
+/**
+ **/
+    Ext.define('Rally.technicalservices.picker.TagTreePicker', {
+        extend: 'Ext.ux.TreeCombo',
+        alias: 'widget.tstagtreepicker',
+
+        componentCls: 'rui-triggerfield rui-project-picker',
+
+        placeholderCls: 'rui-multi-object-placeholder',
+
+        editable: false,
+
+        /**
+         * @cfg {String}
+         * Text shown during initial load
+         */
+        loadingText: 'Loading...',
+        /**
+         * @cfg {String}
+         * Separator to define folder breaks
+         */
+        separator: ':',
+        /**
+         * @cfg {String} displayField
+         * The field inside the model that will be used as the node's text.
+         * Defaults to the default value of {@link Ext.tree.Panel}'s `displayField` configuration.
+         */
+        displayField: 'text',
+        
+        selectChildren: true,
+        
+        canSelectFolders: true,
+
+        multiselect: true,
+
+            
+        initComponent: function () {
+            var me = this;
+            
+            this.store = Ext.create('Ext.data.TreeStore', {
+                root: {}
+            });
+            
+            this._getTagRootNode().then({
+                scope: this,
+                success: function(root) {
+                    this.store.setRootNode(root);
+                },
+                failure: function(message) {
+                    Ext.msg.alert(message);
+                }
+            });
+            
+            me.callParent(arguments);
+        },
+
+        _getTagRootNode: function () {
+            var deferred = Ext.create('Deft.Deferred');
+            var me = this;
+            
+            Ext.create('Rally.data.wsapi.Store',{
+                model: 'tag',
+                autoLoad: true,
+                sorters: [{property:'Name',direction:'ASC'}],
+                filters: [{property:'Archived', value: 'false'}]
+            }).load({
+                callback : function(records, operation, successful) {
+                    if (successful){
+                        var root = this._createRootNode(records);
+                        deferred.resolve(root);
+                    } else {
+                        me.logger.log("Failed: ", operation);
+                        deferred.reject('Problem getting stories: ' + operation.error.errors.join('. '));
+                    }
+                },
+                scope: this
+            });
+            
+            return deferred.promise;
+        },
+        
+        // assume that tag objects are provided in order of Name
+        _createRootNode: function(tags) {
+            var hash = {};
+            
+            Ext.Array.each( tags, function(tag){
+                var name = tag.get('Name');
+                var name_array = name.split(this.separator);
+                
+                hash = this._setHashValue(hash,name,name_array);
+            },this);
+                        
+            var root = {
+                text: 'Tags',
+                expanded: 'true',
+                children: []
+            };
+                
+            Ext.Object.each(hash, function(key,value){
+                root.children.push(value);
+            });
+
+            return root;
+        },
+        
+        _counterValue: 1,
+        
+        _getCounterValue: function() {
+            this._counterValue = this._counterValue + 1;
+            return this._counterValue;
+        },
+        
+        _setHashValue: function(hash,full_name,name_array,parent) {
+            
+            if ( name_array.length > 0 ) {
+                var name = name_array[0];
+                var item_hash = {
+                    id: full_name,
+                    text: name,
+                    children:[],
+                    leaf: true,
+                    checked: false
+                };
+                
+                if( parent ) {
+                    parent.children.push(item_hash);
+                }
+                
+                if ( !parent && !hash[name] ) {
+                    hash[name] = item_hash;
+                }
+                
+                if (name_array.length > 1) {
+                    item_hash.leaf = false;
+                    item_hash.id = name;
+                    name_array.shift();
+                    this._setHashValue(hash,full_name,name_array,hash[name]);
+                }
+            }
+            
+            return hash;
+        }
+    });
--- roadmap_summary/src/javascript/app.js
+++ roadmap_summary/src/javascript/app.js
@@ -2,34 +2,64 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     logger: new Rally.technicalservices.Logger(),
+    
+    scheduleableModel: 'PortfolioItem/Feature',
+    dateField: 'PlannedEndDate',
+    
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
+        {xtype:'container',itemId:'selector_box', margin: 5},
         {xtype:'container',itemId:'display_box'},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
-        this._getRecords("Release", ['Name','ReleaseDate']).then({
-            scope: this,
-            success: function(records){
-                this.logger.log(records);
-                Ext.create('Rally.technicalservices.board.RoadmapBoard',{
-                    width: this.getWidth(),
-                    height: this.getHeight(),
-                    target: this.down('#display_box'),
-                    records: records,
-                    date_field: 'ReleaseDate',
-                    listeners: {
+        this._addSelectors(this.down('#selector_box'));
+    },
+    
+    _addSelectors: function(container) {
+        container.add({ 
+            xtype: 'tstagtreepicker',
+            fieldLabel: 'Use Tags:',
+            listeners: {
+                scope: this,
+                blur: function(picker) {
+                    console.log("HA!",picker.getValue());
+                    var filters = null;
+                    if ( !Ext.isEmpty(picker.getValue()) ){
+                        var values = picker.getValue().split(',');
+                        
+                        var filter_configs = Ext.Array.map(values,function(name){
+                            return { property:'Tags.Name',operator:'contains',value:name };
+                        });
+                        filters = Rally.data.wsapi.Filter.or(filter_configs);
+                    }
+                    this._getRecords(this.scheduleableModel, ['Name',this.dateField], filters).then({
                         scope: this,
-                        clickMarker: this._showReleaseInfo
-                    },
-                    margin: 15
-                },this);
-            },
-            failure: function(error_message){
-                alert(error_message);
+                        success: function(records){
+                            this.logger.log('records',records);
+                            this.down('#display_box').removeAll();
+                            
+                            Ext.create('Rally.technicalservices.board.RoadmapBoard',{
+                                width: this.getWidth(),
+                                height: this.getHeight(),
+                                target: this.down('#display_box'),
+                                records: records,
+                                date_field: this.dateField,
+                                listeners: {
+                                    scope: this,
+                                    clickMarker: this._showReleaseInfo
+                                },
+                                margin: 15
+                            },this);
+                        },
+                        failure: function(error_message){
+                            alert(error_message);
+                        }
+                    });
+                }
             }
         });
     },
+    
     _showReleaseInfo: function(marker,release) {
         this.logger.log("clicked with ", marker, release);
         
@@ -50,19 +80,28 @@ Ext.define('CustomApp', {
             columnCfgs: [{dataIndex:'FormattedID',text:'id'},{dataIndex:'Name',text:'Name'}]
          });
     },
-    _getRecords: function(model_name, model_fields){
+    
+    _getRecords: function(model_name, model_fields, filters){
         var deferred = Ext.create('Deft.Deferred');
         
-//        var context = this.getContext();
-//        if ( model_name == "Release" ) {
-//            context = {
-//                projectScopeDown: false
-//            }
-//        }
+        if ( !filters ) {
+            filters = [{property:'ObjectID',operator:'>',value:0}];
+        }
+        var context = {
+            projectScopeDown: this.getContext().getProjectScopeDown()
+        };
+                
+        if ( model_name == "Release" ) {
+            context = {
+                projectScopeDown: false
+            }
+        }
         Ext.create('Rally.data.wsapi.Store', {
             model: model_name,
             fetch: model_fields,
+            filters: filters,
             autoLoad: true,
+            context: context,
             sorters: [{property: 'Name'}],
             listeners: {
                 load: function(store, records, successful) {
